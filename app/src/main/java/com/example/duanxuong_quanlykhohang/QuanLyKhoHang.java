a6c39c299373161dca751fb541cf8259819bc15c
xbac2912 <nxb2912@gmail.com>
2023-06-28 20:43:55 +0700

    xbac2912-update

--- app/src/main/java/com/example/duanxuong_quanlykhohang/QuanLyKhoHang.java
+++ app/src/main/java/com/example/duanxuong_quanlykhohang/QuanLyKhoHang.java
@@ -1,14 +1,36 @@
 package com.example.duanxuong_quanlykhohang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class QuanLyKhoHang extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quan_ly_kho_hang);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Quản Lý Kho Hàng");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.icon_home);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/duanxuong_quanlykhohang/login.java
+++ app/src/main/java/com/example/duanxuong_quanlykhohang/login.java
@@ -2,7 +2,10 @@ package com.example.duanxuong_quanlykhohang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, QuanLyKhoHang.class);
+                startActivity(intent);
+            }
+        });
     }
 }
