b66e37036c8b42fbcfb85ef2171c049bc7d57437
xbac2912 <nxb2912@gmail.com>
2023-06-28 15:45:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/duanxuong_quanlykhohang/QuanLyKhoHang.java
@@ -0,0 +1,14 @@
+package com.example.duanxuong_quanlykhohang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuanLyKhoHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quan_ly_kho_hang);
+    }
+}
