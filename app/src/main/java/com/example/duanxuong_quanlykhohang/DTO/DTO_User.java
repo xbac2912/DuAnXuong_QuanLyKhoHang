08bcf620eedc6ed8ee17751cfe8b6b039ba72081
Ngocminh241 <72495719+Ngocminh241@users.noreply.github.com>
2023-07-04 19:05:18 +0700

    QuangNgocThuy- add LoaiHang(DAO,DTO)

---
+++ app/src/main/java/com/example/duanxuong_quanlykhohang/DTO/DTO_User.java
@@ -0,0 +1,62 @@
+package com.example.duanxuong_quanlykhohang.DTO;
+
+import java.io.Serializable;
+
+public class DTO_User implements Serializable {
+    private int maND;
+    private String hoTen;
+    private String nguoiDung;
+    private String matKhau;
+    private int vaiTro;
+
+    public DTO_User() {
+    }
+
+    public DTO_User(int maND, String hoTen, String nguoiDung, String matKhau, int vaiTro) {
+        this.maND = maND;
+        this.hoTen = hoTen;
+        this.nguoiDung = nguoiDung;
+        this.matKhau = matKhau;
+        this.vaiTro = vaiTro;
+    }
+
+    public int getMaND() {
+        return maND;
+    }
+
+    public void setMaND(int maND) {
+        this.maND = maND;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getNguoiDung() {
+        return nguoiDung;
+    }
+
+    public void setNguoiDung(String nguoiDung) {
+        this.nguoiDung = nguoiDung;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public int getVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(int vaiTro) {
+        this.vaiTro = vaiTro;
+    }
+}
