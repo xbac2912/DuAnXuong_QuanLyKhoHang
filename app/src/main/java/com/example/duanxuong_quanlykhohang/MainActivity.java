58a04626813b6d98bb9984b92586de4ea370ac85
Thuy Quang <thuyqnph35609@fpt.edu.vn>
2023-09-08 21:08:32 +0700

    update thuy sua dang xuat

--- app/src/main/java/com/example/duanxuong_quanlykhohang/MainActivity.java
+++ app/src/main/java/com/example/duanxuong_quanlykhohang/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(MainActivity.this, login.class);
                 startActivity(intent);
+                finish();
             }
         },3000);
     }
--- app/src/main/java/com/example/duanxuong_quanlykhohang/QuanLyKhoHang.java
+++ app/src/main/java/com/example/duanxuong_quanlykhohang/QuanLyKhoHang.java
@@ -151,8 +151,7 @@ public class QuanLyKhoHang extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
-                Intent intent = new Intent(QuanLyKhoHang.this, login.class);
-                startActivity(intent);
+               finish();
             }
         });
         builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
